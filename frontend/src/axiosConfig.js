fe0b35c456b1d3cd6bd5f04ae5fd8f24f2f2078e
leshliao <leviliao38@gmail.com>
2024-01-09 00:48:14 -0500

    Change baseURL

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'https://food-store-backend-mek5.onrender.com' : '/';
+// axios.defaults.baseURL =
+//   process.env.NODE_ENV !== 'production' ? 'https://food-store-backend-mek5.onrender.com' : '/';
+
+axios.defaults.baseURL = 'https://food-store-backend-mek5.onrender.com';
