c189b6361216ae03b1e3ad3d364cad3778432d0a
leshliao <leviliao38@gmail.com>
2024-01-16 01:41:51 -0500

    trigger action

--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -16,7 +16,7 @@ export const dbconnect = async () => {
     await seedUsers();
     await seedFoods();
     console.log('MongoDB connect successfully!');
-    console.log('test Jan16 0134');
+    console.log('test Jan16 0141');
   } catch (error) {
     console.log(error);
   }
