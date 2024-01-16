247e55c8d1aa2b61235390765f8eb9591c0d7e46
leshliao <leviliao38@gmail.com>
2024-01-16 01:50:12 -0500

    change baseURL

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -4,5 +4,7 @@ import axios from 'axios';
 //  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
 
 // It's because I deploy frontend and backend to different server.(static site and web service)
-axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:4000' : 'https://food-store-backend-mek5.onrender.com';
+// axios.defaults.baseURL =
+//   process.env.NODE_ENV !== 'production' ? 'http://localhost:4000' : 'https://food-store-backend-mek5.onrender.com';
+
+axios.defaults.baseURL = 'http://localhost:4000';
