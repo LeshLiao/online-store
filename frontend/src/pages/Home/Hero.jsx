e2c54091078cb6a96eb6005984977f97bcf3f059
LeviLiao <dlnx38@gmail.com>
2024-02-21 01:29:23 -0500

    Refine layout

--- frontend/src/pages/Home/Hero.jsx
+++ frontend/src/pages/Home/Hero.jsx
@@ -8,11 +8,9 @@ export class Hero extends Component {
       <div className="main">
         <video src={videoBg} autoPlay loop muted playsInline/>
         <div className="content">
-          <br/><br/>
           <div className='its_time_to_text'>IT&apos;S TIME TO</div><br/>
           <div className='change_your_text'>CHANGE</div><br/>
           <div className="wallpaper_text">WALLPAPER</div>
-
         </div>
         {/* <div className='hero'></div> */}
       </div>
