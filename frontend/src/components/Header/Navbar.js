452f58da55da5026f1d0a4d4d144bd48703ba021
LeviLiao <dlnx38@gmail.com>
2024-04-08 19:21:02 -0400

    Fix nav bar css

--- frontend/src/components/Header/Navbar.js
+++ frontend/src/components/Header/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import './Navbar.css'
+// import './Navbar.css'
+import classes from './navbar.module.css'
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded'
 import AccountIcon from '@mui/icons-material/AccountCircleRounded'
 import CartIcon from '@mui/icons-material/ShoppingCartRounded'
@@ -16,9 +17,9 @@ function Navbar () {
 
   useEffect(() => {
     if (click) {
-      document.body.classList.add('fixed-position')
+      document.body.classList.add('fixed_position')
     } else {
-      document.body.classList.remove('fixed-position')
+      document.body.classList.remove('fixed_position')
     }
   }, [click])
 
@@ -41,36 +42,38 @@ function Navbar () {
   return (
     <>
       {/* <div className='top_container' id="top_container"></div> */}
-      <nav className='navbar' id="navbar">
-        <div className='left-container' onClick={handleClick}>
-          <div className='menu-icon'>
+      <nav className={classes.navbar} id="navbar">
+        <div className={classes.left_container} onClick={handleClick}>
+          <div className={classes.menu_icon}>
               <MenuRoundedIcon fontSize='medium'/>
-              <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+              {/* <i className={click ? 'fas fa-times' : 'fas fa-bars'} /> */}
+              <i className={click ? classes.times : classes.bars} /> {/* Use module CSS classes */}
           </div>
           <div></div>
         </div>
-        <div className='center-container'>
+        <div className={classes.center_container}>
           {/* Mobile */}
-          <Link to='/' className='navbar-mobile-logo' onClick={() => { closeMoMenu(); scrollToTop() }}>PaletteX</Link>
+          <Link to='/' className={classes.navbar_mobile_logo} onClick={() => { closeMoMenu(); scrollToTop() }}>PaletteX</Link>
           {/* Desktop */}
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'><Link to='/painting' className='nav-links' onClick={closeMoMenu}>PAINTING</Link></li>
-            <li className='nav-item'><Link to='/landscape' className='nav-links' onClick={closeMoMenu}>LANDSCAPE</Link></li>
+          {/* <ul className={click ? 'nav-menu active' : 'nav-menu'}> */}
+          <ul className={click ? `${classes.nav_menu} ${classes.active}` : classes.nav_menu}>
+            <li className={classes.nav_item}><Link to='/painting' className={classes.nav_links} onClick={closeMoMenu}>PAINTING</Link></li>
+            <li className={classes.nav_item}><Link to='/landscape' className={classes.nav_links} onClick={closeMoMenu}>LANDSCAPE</Link></li>
             {/* <li className='nav-item'><Link to='/gallery' className='nav-links' onClick={closeMoMenu}>Gallery</Link></li> */}
-            <Link to='/' className='navbar-desktop-logo' onClick={() => { closeMoMenu(); scrollToTop() }}>PaletteX</Link>
-            <li className='nav-item'><Link to='/city' className='nav-links' onClick={closeMoMenu}>CITY VIEWS</Link></li>
-            <li className='nav-item'><Link to='/help' className='nav-links' onClick={closeMoMenu}>HELP CENTER</Link></li>
+            <Link to='/' className={classes.navbar_desktop_logo} onClick={() => { closeMoMenu(); scrollToTop() }}>PaletteX</Link>
+            <li className={classes.nav_item}><Link to='/city' className={classes.nav_links} onClick={closeMoMenu}>CITY VIEWS</Link></li>
+            <li className={classes.nav_item}><Link to='/help' className={classes.nav_links} onClick={closeMoMenu}>HELP CENTER</Link></li>
             {/* <li className='nav-item'><Link to='/about'className='nav-links' onClick={closeMoMenu}>About</Link></li> */}
-            <li className='account-icon-mobile'>
-                <Link to='/account' className='account-icon-mobile-link' onClick={closeMoMenu}><AccountIcon /></Link>
+            <li className={classes.account_icon_mobile}>
+                <Link to='/account' className={classes.account_icon_mobile_link} onClick={closeMoMenu}><AccountIcon /></Link>
             </li>
           </ul>
         </div>
-        <div className='right-container'>
-          <div className='login_container'>
-            <Link to='/account' className='account-icon-desktop' onClick={closeMoMenu}><AccountIcon /></Link>
+        <div className={classes.right_container}>
+          <div className={classes.login_container}>
+            <Link to='/account' className={classes.account_icon_desktop} onClick={closeMoMenu}><AccountIcon /></Link>
           </div>
-          <Link to='/cart/view' className='cart-icon' onClick={closeMoMenu}><CartIcon /></Link>
+          <Link to='/cart/view' className={classes.cart_icon} onClick={closeMoMenu}><CartIcon /></Link>
         </div>
       </nav>
     </>
